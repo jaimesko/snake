639b60c9e2909aa2449b10d8f4ed7e2adb26952c
jaimesko <jaimesko96@gmail.com>
2023-11-06 11:39:37 +0100

    Added comments for constants.

--- main.py
+++ main.py
@@ -11,16 +11,19 @@ GREEN = (50, 200, 50)
 BLUE = (50, 50, 200)
 GRID_COLOR = (75, 225, 75)
 
-# Constants
+# Constants for game speed
 FPS = 5
+
+# Constants for cell size
 CELL_SIZE = 20
 
+# Constants for grid dimensions
 X_CELLS = 17
 Y_CELLS = 15
-
 WIDTH = X_CELLS * CELL_SIZE
 HEIGHT = Y_CELLS * CELL_SIZE
 
+# Constants for grid boundaries
 X_MIN = 0
 X_MAX = WIDTH - CELL_SIZE
 Y_MIN = 0
